419ccd409a5fd6cd0382a55eccca3fe5b48b4459
Kenvis <602857267@qq.com>
2020-12-13 19:05:23 +0800

    19:05

--- Senior Programming Practice/36/36.cpp
+++ Senior Programming Practice/36/36.cpp
@@ -1,16 +1,40 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
-
+#include<string>
 using namespace std;
-int ans[501];
+string ans[501];
+
+string addstr(string x, string y)
+{
+    string temp;
+    if(x.length() < y.length()) // ensue that x is longer than y
+    {
+        temp = x;
+        x = y;
+        y = temp;
+    }
+    int i,j;
+    for(i = x.length() - 1, j = y.length() - 1; i >= 0; i--, j--)
+    {
+        x[i] = (x[i] + (j>=0?y[j] - '0' : 0));
+        if(x[i] > '9')
+        {
+            x[i] -= 10;
+            if(i) x[i - 1]++;
+            else x = "1" + x;
+        }
+    }
+    return x;
+}
 
 void find()
 {
-    ans[1] = 1;ans[2] = 2;ans[3] = 4;
+    ans[1] = "1"; ans[2] = "2"; ans[3] = "4";
     for (int i = 4; i <= 500; i++)
     {
-        ans[i] = ans[i - 3] * ans[3] + ans[i - 2] * ans[2] + ans[i - 1] * ans[1];
+        ans[i] = addstr(ans[i - 1],ans[i - 2]);
+        ans[i] = addstr(ans[i],ans[i-3]);
     }
 }
 
@@ -23,5 +47,4 @@ int main()
         if(x == 0) break;
         cout<<ans[x]<<endl;
     }
-    
 }
